d4851fa02a58bb9026cf4f18b7ae4466881ca657
cesareal9 <caugustorama@gmail.com>
2024-09-02 19:07:23 -0300

    alteraçoes 02/09

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express")
 const cep_endereco = require("./middlewares/cep_endereco.js")
 const cliente_controller = require("./controllers/cliente.js")
 const barbeiro_controller = require("./controllers/barbeiro.js")
+const usuario_route = require("./routes/usuario.js")
 const app = express()
 const port = 5000
 
 app.use(express.json())
-// app.use(cep_endereco) // Midleware de uso global
+
 
 app.post("/barbearia", cep_endereco, (req, res) => {
     res.json(req.body)
@@ -60,6 +61,36 @@ app.delete("/barbeiro/:id", (req, res) => {
     res.json()
 })
 
+// cliente 
+
+app.get("/cliente", (req, res) => {
+    res.json(cliente_controller.index())
+})
+
+app.get("/cliente/:id", (req, res) => {
+    res.json(cliente_controller.show(req.params.id))
+})
+
+app.post("/cliente", (req, res) => {
+    const code = cliente_controller.store(req.body)
+    res.status(code).json()
+})
+
+app.put("/cliente/:id", (req, res) => {
+    const code = cliente_controller.update(req.body, req.params.id)
+    res.status(code).json()
+})
+
+app.delete("/cliente/:id", (req, res) => {
+    cliente_controller.destroy(req.params.id)
+    res.json()
+})
+
+// usuario 
+
+app.use("/usuario", usuario_route)
+
+// 
 
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta: ${port} `)
